23d026443ccd06d0debef17534faa8f39086bf80
Ashabul Kahfi <113130339+Ashkh09@users.noreply.github.com>
2023-06-23 14:33:44 +0700

    minggu ke 2
    minggu ke 2

---
+++ ritmic-song-game-main/Assets/unity-ritmic-song-game/Assets/Script/Scene/PindahScene.cs
@@ -0,0 +1,19 @@
+// using System.Collections;
+// using UnityEditor.SceneManagement;
+
+// public class PindahScene : MonoBehaviour
+// {
+//     // Start is called before the first frame update
+//     void Start()
+//     {
+       
+//     }
+
+//     // Update is called once per frame
+//     void Update()
+//     {
+//         if (Input.GetKeyDown(KeyCode.Space)){
+//              UnityEditor.SceneManagement.EditorSceneManager.LoadScene('Game');
+//         }
+//     }
+// }
